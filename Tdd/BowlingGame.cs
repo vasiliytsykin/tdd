7bcb534e0b07783f47b39fe8fad67c80d1aa95d0
vasiliytsykin <vasiliytsykin@gmail.com>
2016-05-31 16:16:03 +0500

    BowlingGame.cs refactored

--- Tdd/BowlingGame.cs
+++ Tdd/BowlingGame.cs
@@ -13,23 +13,16 @@ namespace Kontur.Courses.Testing.Tdd
             Hits = new List<int>(2);
         }
 
-        public List<int> Hits { get; }
-
-        public int FrameScore => Hits.Sum() + BonusPoints;
-
         private int BonusCount => IsStrike ? 2 : IsSpare ? 1 : 0;
-
         private bool IsStrike => Hits.Count > 0 && Hits.First() == 10;
         private bool IsSpare => Hits.Sum() == 10 && !IsStrike;
+
+        public List<int> Hits { get; }
+        public int FrameScore => Hits.Sum() + BonusPoints;
         public bool IsDone => Hits.Count > 1 || IsStrike;
-
         public bool HasBonus => !IsExtraFrame && BonusCount - _givenBonusesCount != 0;
-
         public bool IsExtraFrame { get; set; }
-
         public int BonusPoints { get; private set; }
-
-        public bool IsCorrectRoll(int pins) => Hits.Sum() + pins <= 10;
 
         public void GiveBonus(int pins)
         {
@@ -51,22 +44,37 @@ namespace Kontur.Courses.Testing.Tdd
 
         public Game Roll(int pins)
         {
-            var frs = _frames.ToList();
-            var currentFrame = frs.Last();
-
-            if (currentFrame.IsCorrectRoll(pins))
+            if (IsCorrectRoll(pins))
             {
-                foreach (var frame in frs.Where(f => f.HasBonus))
-                    frame.GiveBonus(pins);
-
-                currentFrame.Hits.Add(pins);
-
-                if (currentFrame.IsDone)
-                    frs.Add(frs.Count < 10 ? new Frame() : new Frame {IsExtraFrame = true});
+                var updatedFrames = UpdateFrames(GiveBonuses(_frames.ToList(), pins), pins);
+                return new Game(updatedFrames.ToImmutableArray());
             }
 
-            return new Game(frs.ToImmutableArray());
+            return this;
         }
+
+        private List<Frame> GiveBonuses(List<Frame> frames, int pins)
+        {
+            foreach (var frame in frames.Where(f => f.HasBonus))
+                frame.GiveBonus(pins);
+
+            return frames;
+        }
+
+        private List<Frame> UpdateFrames(List<Frame> frames, int pins)
+        {
+            var currentFrame = frames.Last();
+            currentFrame.Hits.Add(pins);
+
+            if (currentFrame.IsDone)
+                frames.Add(frames.Count < 10 ? new Frame() : new Frame {IsExtraFrame = true});
+
+            return frames;
+        }
+
+        private bool IsCorrectRoll(int pins) =>      
+             _frames.Last().Hits.Sum() + pins <= 10;
+        
 
         public IList<Frame> GetFrames()
         {
